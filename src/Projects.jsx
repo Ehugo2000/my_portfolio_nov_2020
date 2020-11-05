f529c2ef892b88e74060d1dec94b1ab6734e8d65
ehugo <mihaivos@gmail.com>
2020-11-05 13:48:36 +0100

    Finished projectcard. Everything runs except test for header in project list.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Grid } from "semantic-ui-react";
 import axios from "axios";
+import ProjectCard from './ProjectCard'
 
 
 
@@ -16,7 +17,7 @@ class Projects extends Component {
         let projectsList = projects.map((project) => {
             return (
                 <div id={`project-${project.id}`} key={project.id}>
-                <h3>{project.name}</h3>
+                <ProjectCard project={project} />
                 </div>
             );
         });
@@ -24,7 +25,7 @@ class Projects extends Component {
         return (
             <Container>
                 <h1 id="projects-header">My Projects</h1>
-                {projectsList}
+                <Grid>{projectsList}</Grid>
             </Container>
         );
     }
