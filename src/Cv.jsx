ec7d7dbf7c72198f0a17b1188eea49baee974e12
Ehugo2000 <erik.hgsn@gmail.com>
2020-11-06 17:13:23 +0100

    Add CV text

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -6,8 +6,24 @@ const Cv = () => {
         <Container>
          <h1 id="cv-header">Curriculum vitae</h1>
          <p>
-        CV ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud olore eu fugiat nulla pariatur. 
+         <br></br>
+         <br></br>
+         <br></br>
+        Work experience
+        <br></br>
+        2016 – present: Eckenstam AB, owner. 
+        <br></br>
+        2014 – 2016: Carlsquare, financial analyst.
+        <br></br>
+        Early years: Pig farm & Horse ranch.
+        <br></br>
+        <br></br>
+        Education
+        <br></br>
+        2020: Craft Academy, Fullstack developer.
+        <br></br>
+        2012: Stockholm School of Economics, Master in Economics and Business.
+        
         </p>            
         </Container>
     );
